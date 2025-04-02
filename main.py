11fcb163560309700b13d1cb5a4f6aa063f504ce
HermanGran <142888421+HermanGran@users.noreply.github.com>
2025-04-02 11:51:14 +0200

    added csv file and applied pandas

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Recourses
+# https://www.youtube.com/watch?v=Liv6eeb1VfE
 
+# Path to dataset
+path = "data/WineQT.csv"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Reading Dataset
+df = pd.read_csv(path)
 
+df.info()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
